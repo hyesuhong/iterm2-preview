4de58d6b9fcba500f27428fff45118da57873ad0
Hyesu <honghs95@gmail.com>
2024-10-21 13:52:00 +0900

    feat: change terminal's initial display

--- src/hooks/useTerminal.ts
+++ src/hooks/useTerminal.ts
@@ -5,6 +5,22 @@ interface UseTerminal {
 	options: ITerminalOptions;
 }
 
+const initialDisplay = `
+ ┌ \x1B[01mFeatures\x1B[0m ──────────────────────────────────────────────────────────────────┐\r
+ │                                                                            │\r
+ │ - \x1B[32mSearch\x1B[0m by name                                                           │\r
+ │                                                                            │\r
+ │ - \x1B[34mFilter\x1B[0m by appearance                                                     │\r
+ │                                                                            │\r
+ │ - \x1B[35mDownload\x1B[0m if you find a scheme you like                                   │\r
+ │                                                                            │\r
+ └────────────────────────────────────────────────────────────────────────────┘\r
+ \r
+ This sample is a simulator.\r
+ Try running 'help', then you can check available commands.\r
+ \r
+ $ `;
+
 const useTerminal = <T extends HTMLElement>({ options }: UseTerminal) => {
 	const terminalRef = useRef<Terminal | null>(null);
 	const wrapperRef = useRef<T>(null);
@@ -14,7 +30,7 @@ const useTerminal = <T extends HTMLElement>({ options }: UseTerminal) => {
 			const terminal = new Terminal(options);
 
 			terminal.open(wrapperRef.current);
-			terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ \r\n$ ');
+			terminal.write(initialDisplay);
 			terminal.onData((data) => {
 				const targetCode = data.charCodeAt(0);
 				if (targetCode === 13) {
