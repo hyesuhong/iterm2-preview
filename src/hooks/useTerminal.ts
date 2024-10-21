88d76fa03294fe3fa9aa3083f600ef459a9e1e29
Hyesu <honghs95@gmail.com>
2024-10-21 15:16:30 +0900

    feat: change terminal event(onData -> onKey)

--- src/hooks/useTerminal.ts
+++ src/hooks/useTerminal.ts
@@ -5,25 +5,38 @@ interface UseTerminal {
 	options: ITerminalOptions;
 }
 
-const initialDisplay = `
- ┌ \x1B[01mFeatures\x1B[0m ──────────────────────────────────────────────────────────────────┐\r
- │                                                                            │\r
- │ - \x1B[32mSearch\x1B[0m by name                                                           │\r
- │                                                                            │\r
- │ - \x1B[34mFilter\x1B[0m by appearance                                                     │\r
- │                                                                            │\r
- │ - \x1B[35mDownload\x1B[0m if you find a scheme you like                                   │\r
- │                                                                            │\r
- └────────────────────────────────────────────────────────────────────────────┘\r
- \r
- This sample is a simulator.\r
- Try running 'help', then you can check available commands.\r
- \r
- $ `;
+const initialDisplay = `\r
+┌ Features ───────────────────────────────────┐\r
+│                                             │\r
+│ - \x1B[32mSearch\x1B[0m by name                            │\r
+│                                             │\r
+│ - \x1B[34mFilter\x1B[0m by appearance                      │\r
+│                                             │\r
+│ - \x1B[35mDownload\x1B[0m if you find a scheme you like    │\r
+│                                             │\r
+└─────────────────────────────────────────────┘\r
+\r
+This sample is a simulator.\r
+Try running 'help', then you can check available commands.\r
+\r
+$ `;
 
 const useTerminal = <T extends HTMLElement>({ options }: UseTerminal) => {
 	const terminalRef = useRef<Terminal | null>(null);
 	const wrapperRef = useRef<T>(null);
+	const commandRef = useRef('');
+
+	const getCommand = () => {
+		console.log(commandRef);
+		const totalCommandLine = commandRef.current;
+		const [commandOnly] = totalCommandLine.split(' ');
+
+		return commandOnly;
+	};
+
+	const setCommand = (newCommand: string) => {
+		commandRef.current = newCommand;
+	};
 
 	useEffect(() => {
 		if (wrapperRef.current) {
@@ -31,13 +44,28 @@ const useTerminal = <T extends HTMLElement>({ options }: UseTerminal) => {
 
 			terminal.open(wrapperRef.current);
 			terminal.write(initialDisplay);
-			terminal.onData((data) => {
-				const targetCode = data.charCodeAt(0);
-				if (targetCode === 13) {
-					terminal.write('\r\n$ ');
+			terminal.onKey(({ domEvent }) => {
+				const { key } = domEvent;
+				if (key === 'Enter') {
+					const command = getCommand();
+					// do something with command
+					// terminal.write('\r\n$ ');
+
+					setCommand('');
+					return;
+				} else if (key === 'Backspace') {
+					terminal.write('\b \b');
+					const slicedCommand = commandRef.current.slice(
+						0,
+						commandRef.current.length - 1
+					);
+
+					setCommand(slicedCommand);
 					return;
 				}
-				terminal.write(data);
+
+				terminal.write(key);
+				setCommand(commandRef.current + key);
 			});
 
 			terminalRef.current = terminal;
